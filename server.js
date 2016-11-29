4d7bed0e1ba9a9676d8162a79575e63f816eb1fb
Aidan Baack <aebaack@gmail.com>
2016-11-29 10:00:00 -0700

    Add simple server functionality

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const port = 8000;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
